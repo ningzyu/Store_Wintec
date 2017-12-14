5778c092c71fbbcfccb232bffa0a990ba7e35381
bidizi <869434884@qq.com>
2017-12-14 15:13:26 +0800

    2017/12/14  jiangtianyu

--- app/src/main/java/com/nzy/zkyt/store_wintec/ui/fragment/MyFragment.java
+++ app/src/main/java/com/nzy/zkyt/store_wintec/ui/fragment/MyFragment.java
@@ -1,13 +1,75 @@
 package com.nzy.zkyt.store_wintec.ui.fragment;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.nzy.zkyt.store_wintec.R;
 import com.nzy.zkyt.store_wintec.ui.MainActivity;
 import com.nzy.zkyt.store_wintec.ui.base.BaseFragment;
 import com.nzy.zkyt.store_wintec.ui.presenter.FgMyPresenter;
 import com.nzy.zkyt.store_wintec.ui.view.FgMyView;
 
+import butterknife.BindView;
 
-public class MyFragment  extends BaseFragment<FgMyView, FgMyPresenter> implements FgMyView {
+
+public class MyFragment  extends BaseFragment<FgMyView, FgMyPresenter> implements FgMyView, View.OnClickListener {
+
+    @BindView(R.id.image_manage_search)
+    ImageView btn_manage_search;
+    @BindView(R.id.btn_update_manage)
+    LinearLayout btn_update_manage;
+    @BindView(R.id.btn_apk_manage)
+    LinearLayout btn_apk_manage;
+    @BindView(R.id.btn_install_manage)
+    LinearLayout btn_install_manage;
+    @BindView(R.id.btn_clear_manage)
+    LinearLayout btn_clear_manage;
+    @BindView(R.id.btn_setting_manage)
+    LinearLayout btn_setting_manage;
+
+    @Override
+    public void initData() {
+    }
+
+    @Override
+    public void initListener() {
+//        btn_manage_search.setOnClickListener(this);
+//        btn_update_manage.setOnClickListener(this);
+    }
+
+    /**
+     * 按键监听
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.image_manage_search:
+                break;
+
+            case R.id.btn_apk_manage:
+                break;
+
+            case R.id.btn_clear_manage:
+                break;
+
+            case R.id.btn_install_manage:
+                break;
+
+            case R.id.btn_update_manage:
+                Log.i("ab",">>>>>>>>>>>>");
+                break;
+
+            case R.id.btn_setting_manage:
+                break;
+        }
+    }
+
     @Override
     protected FgMyPresenter createPresenter() {
         return new FgMyPresenter((MainActivity) getActivity());
@@ -17,4 +79,33 @@ public class MyFragment  extends BaseFragment<FgMyView, FgMyPresenter> implement
         return R.layout.fragment_my;
     }
 
+    @Override
+    public ImageView getSearchBtn() {
+        return btn_manage_search;
+    }
+
+    @Override
+    public LinearLayout getUpdateManageBtn() {
+        return btn_update_manage;
+    }
+
+    @Override
+    public LinearLayout getInstallManageBtn() {
+        return btn_install_manage;
+    }
+
+    @Override
+    public LinearLayout getApkManageBtn() {
+        return btn_apk_manage;
+    }
+
+    @Override
+    public LinearLayout getClearBtn() {
+        return btn_clear_manage;
+    }
+
+    @Override
+    public LinearLayout getSettingBtn() {
+        return btn_setting_manage;
+    }
 }
--- app/src/main/java/com/nzy/zkyt/store_wintec/ui/view/FgMyView.java
+++ app/src/main/java/com/nzy/zkyt/store_wintec/ui/view/FgMyView.java
@@ -1,5 +1,8 @@
 package com.nzy.zkyt.store_wintec.ui.view;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 /**
  * 作者：宁震宇on 2017/11/30.
  * 邮箱：348723352@qq.com
@@ -7,4 +10,10 @@ package com.nzy.zkyt.store_wintec.ui.view;
  */
 
 public interface FgMyView {
+    ImageView getSearchBtn();
+    LinearLayout getUpdateManageBtn();
+    LinearLayout getInstallManageBtn();
+    LinearLayout getApkManageBtn();
+    LinearLayout getClearBtn();
+    LinearLayout getSettingBtn();
 }
